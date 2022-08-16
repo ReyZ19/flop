7fcb080032d3a1ae4abcff3f58d3f76bd4fced08
ReyZ19 <reyztr@protonmail.ch>
2022-08-17 01:26:09 +0300

    jbh.rocks(floppa api) yemişler

--- cmds/atatürk.js
+++ cmds/atatürk.js
@@ -7,12 +7,15 @@ const axios = require('axios');
  * @param {Map} map
  */
 module.exports.run = async (bot, msg) => {
-	axios('https://ataturk.vercel.app/tr').then((res) => {
-		var random = Math.floor(Math.random() * 100) + 1;
-		axios(`https://ataturk-resimleri-api.herokuapp.com/images/${random}.png`, { responseType: 'arraybuffer' }).then((res1) => {
-			msg.reply({ content: '> ' + res.data.quote, files: [res1.data], allowedMentions: { repliedUser: false } });
+	await msg.reply({ content: 'Bekle...', allowedMentions: { repliedUser: false } }).then(message => {
+		axios('https://ataturk.vercel.app/tr').then((res) => {
+			var random = Math.floor(Math.random() * 100) + 1;
+			axios(`https://ataturk-resimleri-api.herokuapp.com/images/${random}.png`, { responseType: 'arraybuffer' }).then((res1) => {
+				msg.reply({ content: '> ' + res.data.quote, files: [res1.data], allowedMentions: { repliedUser: false } });
+				message.delete();
+			});
 		});
-	});
+	})
 }
 
 module.exports.help = {
